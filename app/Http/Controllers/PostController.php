b8d4729a0957c085c5a357812b51fdbd93f11c9a
satriasuryan <satriasuryatria@gmail.com>
2018-05-25 14:34:05 +0700

    Buat Method Show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -72,7 +72,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        return view('posts.show');
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
+
     }
 
     /**
