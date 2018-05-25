f526b81a1e68376d44e69a77263956f2e660bc0a
satriasuryan <satriasuryatria@gmail.com>
2018-05-25 16:07:54 +0700

    buat method destroy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -119,6 +119,9 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect()->route('posts.index');
     }
 }
